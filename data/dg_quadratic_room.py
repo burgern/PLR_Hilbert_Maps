b9bbf91b132adf70c525aee72475f9536c68a3a1
Nico Burger <nicoburger1998@gmail.com>
2022-03-17 23:20:44 +0100

    Data Generator added shuffle and split

--- data/dg_quadratic_room.py
+++ data/dg_quadratic_room.py
@@ -11,18 +11,40 @@ class DataGeneratorRoom:
             n_free: number of requested non-occupied sample points
             noise_mean: mean of gaussian noise
             noise_std: standard deviation of gaussian noise
+            train_split: fraction of dataset which should be training [0, 1]
     """
-    def __init__(self, length, n_occ, n_free, noise_mean, noise_std):
+    def __init__(self, length, n_occ, n_free, noise_mean, noise_std, train_split):
         # input parameters
         self.length = length
         self.n_occ = n_occ
         self.n_free = n_free
         self.noise_mean = noise_mean
         self.noise_std = noise_std
+        self.train_split = train_split
 
         # data generation
         self.X_occ, self.y_occ = self.generate_occupied()
         self.X_free, self.y_free = self.generate_free()
+        self.X = np.append(self.X_occ, self.X_free, axis=0)
+        self.y = np.append(self.y_occ, self.y_free, axis=0)
+
+        # data shuffling
+        self.shuffle()
+
+        # split train and evaluation dataset
+        train_split_idx = int(self.train_split * self.X.shape[0])
+        self.X_train = self.X[:train_split_idx, :]
+        self.X_test = self.X[train_split_idx:, :]
+        self.y_train = self.y[:train_split_idx]
+        self.y_test = self.y[train_split_idx:]
+
+        # convert to pytorch format for DataLoader
+        # TODO
+
+    def shuffle(self):
+        assert len(self.X) == len(self.y), "Data generation failed, X and y to not have same size"
+        p = np.random.permutation(len(self.X))
+        self.X, self.y = self.X[p], self.y[p]
 
     def generate_occupied(self):
         """ x: [x_pos, y_pos] """
@@ -69,5 +91,5 @@ class DataGeneratorRoom:
         plt.show()
 
 
-dg = DataGeneratorRoom(10, 1000, 1000, 0, 0.1)
+dg = DataGeneratorRoom(10, 1000, 1000, 0, 0.1, 0.9)
 dg.visualize()
