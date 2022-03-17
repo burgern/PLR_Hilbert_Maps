8d2f204ac575421b8694c525e87f038a6e4640bd
Nico Burger <nicoburger1998@gmail.com>
2022-03-17 16:22:21 +0100

    train and test functions for local_trainer

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from . import loss
 from . import models
+from . import training
 from . import utils
--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,32 @@
+import torch
 from PLR_Hilbert_Maps.utils import device_setup
 from PLR_Hilbert_Maps.models import MlpLocal2D
+from PLR_Hilbert_Maps.loss import HilbertMapLoss
+from PLR_Hilbert_Maps.training import train, test
 
 
 def main():
+    # Set up device
     device = device_setup()
+
+    # Load data
+    # TODO
+
+    # Load model
     model = MlpLocal2D().to(device)
     print(model)
+
+    # Loss and optimizer
+    loss_fn = HilbertMapLoss().loss_fn
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    # train
+    # epochs = 5
+    # for t in range(epochs):
+    #     print(f"Epoch {t+1}\n-------------------------------")
+    #     train(train_dataloader, model, device, loss_fn, optimizer)
+    #     test(test_dataloader, model, device, loss_fn)
+    # print("Done!")
 
 
 if __name__ == "__main__":
