d9815d6be2eca8cbb74421cecd470f41365aa5a3
Nico Burger <nicoburger1998@gmail.com>
2022-03-18 00:20:12 +0100

    add data generation to main program

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,10 @@
 import torch
+from torch.utils.data import DataLoader
 from PLR_Hilbert_Maps.utils import device_setup
 from PLR_Hilbert_Maps.models import MlpLocal2D
 from PLR_Hilbert_Maps.loss import HilbertMapLoss
 from PLR_Hilbert_Maps.training import train, test
+from PLR_Hilbert_Maps.data import DataGeneratorRoom
 
 
 def main():
@@ -10,7 +12,12 @@ def main():
     device = device_setup()
 
     # Load data
-    # TODO
+    batch_size = 32
+    data_generator = DataGeneratorRoom(length=10, n_occ=1000, n_free=4000,
+                                       noise_mean=0, noise_std=0.2, train_split=0.9)
+    train_dataloader = DataLoader(data_generator.train, batch_size=batch_size)
+    test_dataloader = DataLoader(data_generator.test, batch_size=batch_size)
+    data_generator.visualize()  # visualize created data
 
     # Load model
     model = MlpLocal2D().to(device)
