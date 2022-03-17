2cb227bc2b052573b71d3e491115b7fd07f661f8
Nico Burger <nicoburger1998@gmail.com>
2022-03-17 14:10:11 +0100

    device setup

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .device_setup import device_setup
---
+++ utils/device_setup.py
@@ -0,0 +1,4 @@
+def device_setup(device: str = "cpu"):
+    """ Get cpu or gpu device for training. """
+    print(f"Using {device} device")
+    return device
