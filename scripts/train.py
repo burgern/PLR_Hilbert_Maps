898d1bdf8ba33713a609768f5a7ca33148f8cf46
Nico Burger <nicoburger1998@gmail.com>
2022-03-20 00:26:38 +0100

    update training

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from PLR_Hilbert_Maps.utils import device_setup
-from PLR_Hilbert_Maps.models import MlpLocal2D
+from PLR_Hilbert_Maps.models import MlpLocal2D, FCN
 from PLR_Hilbert_Maps.loss import HilbertMapLoss
 from PLR_Hilbert_Maps.training import train, test
 from PLR_Hilbert_Maps.data import DataGeneratorRoom
@@ -20,15 +20,13 @@ def main():
                                        noise_mean=0, noise_std=0.1, train_split=1.0)
     train_dataloader = DataLoader(data_generator.train, batch_size=batch_size)
     test_dataloader = DataLoader(data_generator.test, batch_size=batch_size)
-    data_generator.visualize()  # visualize created data
 
     # Load model
     model = MlpLocal2D().to(device)
-    print(model)
 
     # Loss and optimizer
     loss_fn = HilbertMapLoss().loss_fn
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     # train
     epochs = 30
