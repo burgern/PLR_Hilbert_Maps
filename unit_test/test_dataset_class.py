e4ef619fd72078bcc08fce3d7ba8bd0c8425ad8e
hadcamir <hadzica@student.ethz.ch>
2022-03-30 16:56:22 +0200

    Add unit test for testing the 2d replica dataset class.

---
+++ unit_test/test_dataset_class.py
@@ -0,0 +1,34 @@
+import argparse
+from data.replica_dataset import ReplicaDataSet
+import json
+import matplotlib.pyplot as plt
+from numpy.random import randint
+from numpy import concatenate
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test the replica dataset class")
+    parser.add_argument('--config', action="store")
+    parser.add_argument('--number_of_viewpoints', action="store")
+
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config = json.load(f)
+
+    dataset = ReplicaDataSet(config)
+
+    idx = randint(dataset.__len__(), size=int(args.number_of_viewpoints))
+
+    pc_room = []
+    for id in idx:
+        pc_scan = dataset.__getitem__(id)
+        pc_room.append(pc_scan)
+    pc_room = concatenate(pc_room, axis=1)
+    plt.scatter(pc_room[0], pc_room[1], c=pc_room[2], s=0.01)
+    plt.show()
+
+
+
+
+
+
+
