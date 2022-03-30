8124f66efc150604bebffc38bbb5554385119402
hadcamir <hadzica@student.ethz.ch>
2022-03-30 16:55:50 +0200

    Bugfixes in 2d replica dataset class.

--- data/replica_dataset.py
+++ data/replica_dataset.py
@@ -1,15 +1,15 @@
 from data.base_dataset import BaseDataSet
 from utils.data_utils import get_all_files_from_directory, get_depth_samples_and_pose_data_from_replica_data_folder
 from os.path import join
-from numpy import arange, tan, loadtxt, ones, vstack, load
-from utils.math_utils import sample_sensor_row_indices
+from numpy import arange, loadtxt, ones, vstack, load, sin, cos, concatenate
+from utils.math_utils import sample_angles_around_optical_axis, sample_points_in_freespace_for_viewpoint, \
+    point_cloud_WRT_World, deg_to_rad
 
 class ReplicaDataSet(BaseDataSet):
     def __init__(self, config):
         super().__init__(config)
         self.sensor_fov = self.sensor_config["theta_fov_deg"]
         self.sensor_W = self.sensor_config["W_px"]
-        self.sensor_focal_length = self.sensor_W / (2 * tan(self.sensor_fov / 2))
         self.sensor_angular_resolution = self.sensor_fov / self.sensor_W
         self.data_type = self.data_config["type"]
         self.data_folder_path = self.data_config["path"]
@@ -32,14 +32,17 @@ class ReplicaDataSet(BaseDataSet):
             depth_sample_path = join(self.data_folder_path, self.depth_samples[item])
             with open(depth_sample_path, "rb") as f:
                 depth_data = load(f, allow_pickle=True)
-            sensor_row_indices = sample_sensor_row_indices(self.sensor_W)
-            x = depth_data * sensor_row_indices / self.sensor_focal_length
-            y = depth_data
+            sensor_pixel_angles = sample_angles_around_optical_axis(self.sensor_fov, self.sensor_angular_resolution)
+            sensor_pixel_angles = deg_to_rad(sensor_pixel_angles)
+            x = depth_data * cos(sensor_pixel_angles)
+            y = depth_data * sin(sensor_pixel_angles)
+            points_in_free_space = sample_points_in_freespace_for_viewpoint([x, y])
             pose_data = self.pose_data[item, :] * self.square_unit_m
-            x = x + pose_data[1]
-            y = y + pose_data[0]
             p = ones(x.shape)
             point_cloud_data = vstack([x, y, p])
+            point_cloud_data = point_cloud_WRT_World([pose_data[1], pose_data[0]], point_cloud_data)
+            points_in_free_space = point_cloud_WRT_World([pose_data[1], pose_data[0]], points_in_free_space)
+            point_cloud_data = concatenate((point_cloud_data, points_in_free_space), axis=1)
         else:
             raise NotImplementedError
         # TODO hadzcamir: Sample and add non occupied points.
