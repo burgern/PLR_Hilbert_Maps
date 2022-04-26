a8fb2fd7552cd960ef07c0f7a994172f3ffefdda
hadcamir <hadzica@student.ethz.ch>
2022-04-26 16:47:54 +0200

    [Merge] Remove wrongly merged code

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD:src/data/__init__.py
 from .dg_quadratic_room import DataGeneratorRoom
 from .dataset_elements import DatasetHexagon
-=======
-#from .dg_quadratic_room import DataGeneratorRoom
-#from .grid_manger import GridManager
->>>>>>> hadzica/hack_branch:data/__init__.py
