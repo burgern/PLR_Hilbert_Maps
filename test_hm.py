94319616098a99e620d68d6f09949d0428441954
hadcamir <hadzica@student.ethz.ch>
2022-05-02 13:04:55 +0200

    [Repo Management] Remove ini configs

---
+++ test_hm.py
@@ -0,0 +1,14 @@
+from src.hilbert_map.hilbert_map import HilbertMap
+import argparse
+
+def main(args):
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test the replica dataset class")
+    parser.add_argument('--config', action="store")
+
+    args = parser.parse_args()
+    main(args)
+
