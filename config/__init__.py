25e16884d679dec7db015cd4b83ca19fa6a3b9aa
Nico Burger <nicoburger1998@gmail.com>
2022-05-10 11:17:18 +0200

    [CONFIG] added loading and additional parameters

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,5 @@
 from .paths import *
-from .config import read_config
+from .config import read_config, load_config
 
 __project_path__ = read_config('config.ini', 'Setup', 'project_path')
 __dataset_path__ = read_config('config.ini', 'Setup', 'dataset_path')
--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 import configparser
 from config import PATH_CONFIG
 import os
+import json
 
 
 def read_config(config_file, section, key):
@@ -11,3 +12,10 @@ def read_config(config_file, section, key):
     except KeyError:
         raise ValueError(f"{config_file} file is missing key '{key}'")
     return ret
+
+
+def load_config(config_file: str = "test_hilbert_map_config.json"):
+    config_file = os.path.join(PATH_CONFIG, config_file)
+    with open(config_file) as file:
+        config = json.load(file)
+    return config
